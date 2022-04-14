c37b2b887a1e7cf3b9ed5d0f8255148468e5cef3
Albert Jonathan <borjajonathann@gmail.com>
2022-04-14 13:11:39 +0800

    first commit on class27

---
+++ objects-netflix-tv-shows/js/main.js
@@ -0,0 +1,20 @@
+//Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
+
+
+class TVShow{
+  constructor(showName, numberOfEpisodes, releaseDate, showGenre){
+    this.showName = showName
+    this.numberOfEpisodes = numberOfEpisodes
+    this.releaseDate = releaseDate
+    this.showGenre = showGenre
+  }
+  playShow(){
+    console.log('Play')
+  }
+  pauseShow(){
+    console.log('Pause.')
+  }
+  reminderToWatchShow(){
+    console.log(`Time to watch ${this.showName}`)
+  }
+}
