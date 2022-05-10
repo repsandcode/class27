b2ffe22b0becf8f0bd8e085d639bd40304dd35ce
Albert Jonathan <borjajonathann@gmail.com>
2022-05-10 08:24:03 +0800

    new js code

--- local-storage-botScore-button/js/main.js
+++ local-storage-botScore-button/js/main.js
@@ -1,3 +1,10 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
 
+document.querySelector('button').addEventListener('click', addOne)
+
+function addOne() {
+  const show = document.querySelector('h2')
+
+  show.innerText = +(show) + 1
+}
 
