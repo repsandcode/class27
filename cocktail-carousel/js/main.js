a84c61f4acae78eb279d88296b8fa3e78158a84d
Albert Jonathan <borjajonathann@gmail.com>
2022-05-13 16:02:19 +0800

    carousel: finsihed automatic carousel

--- cocktail-carousel/js/main.js
+++ cocktail-carousel/js/main.js
@@ -1,4 +1,5 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+const carousel = document.querySelector(".carousel");
 document.querySelector('button').addEventListener('click', getDrink)
 
 function getDrink() {
@@ -6,14 +7,32 @@ function getDrink() {
   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`
 
   fetch(url)
-
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      console.log(data)
-      document.querySelector('h2').innerText = data.drinks[0].strDrink
-      document.querySelector('img').src = data.drinks[0].strDrinkThumb
-      document.querySelector('h3').innerText = data.drinks[0].strInstructions    
+      console.log(data.drinks)
+
+      let index = 0;
+      const len = data.drinks.length
+      
+      setInterval(function() {
+        startCarousel();
+      }, 3000);
+
+      const startCarousel = () => {
+        document.querySelector('h2').innerText = data.drinks[index].strDrink
+        document.querySelector('img').src = data.drinks[index].strDrinkThumb
+        document.querySelector('h3').innerText = data.drinks[index].strInstructions
+        console.log(index)
+
+        index++
+        
+        carousel.classList.remove("fade");
+        void carousel.offsetWidth;
+        carousel.classList.add("fade");
+        if(index > len - 1) index = 0;
+      }
     })
+
     .catch(err => {
         console.log(`error ${err}`)
     });
