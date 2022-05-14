586729ce9b2cd4cc299b527ceb6bb9a8225e81f8
Albert Jonathan <borjajonathann@gmail.com>
2022-05-14 15:56:37 +0800

    weather: refined js code

--- weather-api/js/main.js
+++ weather-api/js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = 'http://api.weatherstack.com/forecast?access_key=3d043bf2f574dcc5e44af77a9e104994&query='+choice
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
