72eb74bb40bf9dc28cb101169cc6c7b44a83902d
Albert Jonathan <borjajonathann@gmail.com>
2022-05-14 17:41:33 +0800

    weather: added geolocation api and fixed id-names

--- weather-api/js/main.js
+++ weather-api/js/main.js
@@ -1,5 +1,38 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('#location').addEventListener('click', getFetch)
+
+// const url = 'http://api.weatherstack.com/forecast?access_key=3d043bf2f574dcc5e44af77a9e104994&query='+choice
+
+
+// fetch(url)
+//   .then(res => res.json()) // parse response as JSON
+//   .then(data => {
+//     console.log(data)
+//   })
+//   .catch(err => {
+//     console.log(`error ${err}`)
+//   });
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+} else {
+  alert('Your Browser does not support Geolocation')
+}
+
+let latitude = 0
+let longitude = 0 
+function successCallback(position) {
+  latitude = position.coords.latitude
+  longitude = position.coords.longitude
+  
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+}
+
+function errorCallback(error){
+  console.log(error.message);
+}
+console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
 
 function getFetch(){
   const choice = document.querySelector('input').value
