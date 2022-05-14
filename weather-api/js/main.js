ad1305aa64180fdb0ff9bb2ef9a1d8997965b292
Albert Jonathan <borjajonathann@gmail.com>
2022-05-14 21:10:45 +0800

    weather-api: removed form element in html file

--- weather-api/js/main.js
+++ weather-api/js/main.js
@@ -1,5 +1,5 @@
 //Example fetch using pokemonapi.co
-document.querySelector('#location').addEventListener('click', getFetch)
+document.querySelector('#search').addEventListener('click', getFetch)
 
 // const url = 'http://api.weatherstack.com/forecast?access_key=3d043bf2f574dcc5e44af77a9e104994&query='+choice
 
@@ -24,7 +24,7 @@ let longitude = 0
 function successCallback(position) {
   latitude = position.coords.latitude
   longitude = position.coords.longitude
-  
+
   console.log(position.coords.latitude);
   console.log(position.coords.longitude);
 }
