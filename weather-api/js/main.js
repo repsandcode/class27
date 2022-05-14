7abd38f121711dd1bea3776ef6794a28efd5c019
Albert Jonathan <borjajonathann@gmail.com>
2022-05-14 21:35:36 +0800

    weather-api: modified html code and added another fetch API code to main.js

--- weather-api/js/main.js
+++ weather-api/js/main.js
@@ -1,39 +1,53 @@
-//Example fetch using pokemonapi.co
+// event listener to run ONLY WHEN THERE IS AN LOCATION INPUT
 document.querySelector('#search').addEventListener('click', getFetch)
 
-// const url = 'http://api.weatherstack.com/forecast?access_key=3d043bf2f574dcc5e44af77a9e104994&query='+choice
+// initialize the variables needed for URL queries
+let latitude = 0
+let longitude = 0 
 
-
-// fetch(url)
-//   .then(res => res.json()) // parse response as JSON
-//   .then(data => {
-//     console.log(data)
-//   })
-//   .catch(err => {
-//     console.log(`error ${err}`)
-//   });
-
+// check if the user's browser is compatible for geolocation
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 } else {
   alert('Your Browser does not support Geolocation')
 }
 
-let latitude = 0
-let longitude = 0 
+
 function successCallback(position) {
-  latitude = position.coords.latitude
-  longitude = position.coords.longitude
+  latitude = (position.coords.latitude).toFixed(5)
+  longitude = (position.coords.longitude).toFixed(5)
 
   console.log(position.coords.latitude);
   console.log(position.coords.longitude);
+  console.log('this is the latitude (up to 5 decimal places) '+latitude);
+  console.log('this is the longitude (up to 5 decimal places) '+longitude);
 }
 
 function errorCallback(error){
   console.log(error.message);
 }
-console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
 
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+console.log(timezone)
+const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&timezone=${timezone}`
+const temperature = document.querySelector('#temperature')
+const windSpeed = document.querySelector('#wind-speed')
+const windDirection = document.querySelector('#wind-direction')
+
+fetch(url)
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    console.log(data)
+    temperature.innerText = data.current_weather.temperature
+    windSpeed.innerText = data.current_weather.windspeed + " km/h"
+    windDirection.innerText = data.current_weather.winddirection
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  });
+
+
+// THIS FETCH API will only run if button is clicked
 function getFetch(){
   const choice = document.querySelector('input').value
   const url = 'http://api.weatherstack.com/forecast?access_key=3d043bf2f574dcc5e44af77a9e104994&query='+choice
