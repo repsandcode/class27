c1cb6d846041b00240cdc7a498f32b9ebf58be43
Albert Jonathan <borjajonathann@gmail.com>
2022-05-17 09:54:57 +0800

    weather-api: CSS -> fixed ul

--- weather-api/js/main.js
+++ weather-api/js/main.js
@@ -6,42 +6,46 @@ let latitude = 0
 let longitude = 0 
 
 // check if the user's browser is compatible for geolocation
+// this will prompt the user to turn on their device's location mode
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 } else { 
   alert('Your Browser does not support Geolocation')
 }
 
-
+// get the latitude and longtitude of your current location
 function successCallback(position) {
-  latitude = position.coords.latitude
-  longitude = position.coords.longitude
+  latitude = position.coords.latitude.toFixed(2)
+  longitude = position.coords.longitude.toFixed(2)
 
   console.log(position.coords.latitude);
   console.log(position.coords.longitude);
-  console.log('this is the latitude (up to 5 decimal places) '+latitude);
-  console.log('this is the longitude (up to 5 decimal places) '+longitude);
+  console.log('this is the latitude (up to 5 decimal places) -> '+latitude);
+  console.log('this is the longitude (up to 5 decimal places) -> '+longitude);
 }
 
 function errorCallback(error){
   console.log(error.message);
 }
 
-// // timezone
-// const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
-// console.log(timezone)
-const url = `https://www.metaweather.com/api/location/search/?lattlong=${latitude},${longitude}`
-const temperature = document.querySelector('#temperature')
-const windSpeed = document.querySelector('#wind-speed')
-const windDirection = document.querySelector('#wind-direction')
+// timezone
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+console.log(timezone)
+// url needed to get data
+const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&timezone=${timezone}`
+const weatherCode = document.querySelector('#weather-code')
+const temperature = document.querySelector('#temperature');
+const windSpeed = document.querySelector('#wind-speed');
+const windDirection = document.querySelector('#wind-direction');
 
 fetch(url)
   .then(res => res.json()) // parse response as JSON
   .then(data => {
     console.log(data)
-    // temperature.innerText = data.current_weather.temperature
-    // windSpeed.innerText = data.current_weather.windspeed + " km/h"
-    // windDirection.innerText = data.current_weather.winddirection
+    weatherCode.innerText = weatherCodes(data.current_weather.weathercode)
+    temperature.innerText = data.current_weather.temperature
+    windSpeed.innerText = data.current_weather.windspeed + " km/h"
+    windDirection.innerText = data.current_weather.winddirection
   })
   .catch(err => {
       console.log(`error ${err}`)
@@ -61,4 +65,39 @@ function getFetch(){
       .catch(err => {
           console.log(`error ${err}`)
       });
+}
+
+
+// function will return description of a specific weather code
+function weatherCodes(code) {
+  switch (code) {
+    case 0: return "Clear sky"
+    case 1: return "Mainly clear"
+    case 2: return "Partly cloudy" 
+    case 3: return "Overcast"
+    case 45: return "Fog"
+    case 48: return "Depositing rime fog"
+    case 51: return "Drizzle: Light"
+    case 53: return "Drizzle: Moderate"
+    case 55: return "Drizzle: Dense Intensity"
+    case 56: return "Freezing Drizzle: Light Intensity"
+    case 57: return "Freezing Drizzle: Dense Intensity"
+    case 61: return "Rain: Slight"
+    case 63: return "Rain: Moderate"
+    case 65: return "Rain: Heavy"
+    case 66: return "Freezing Rain: Light Intensity"
+    case 67: return "Freezing Rain: Heavy Intensity"
+    case 71: return "Slight Snow Fall"
+    case 73: return "Moderate Snow Fall"
+    case 75: return "Heavy Snow Fall"
+    case 77: return "Snow grains"
+    case 80: return "Slight rain showers"
+    case 81: return "Moderate rain showers"
+    case 82: return "Heavy rain showers"
+    case 85: return "Slight snow showers"
+    case 86: return "Heavy snow showers"
+    case 95: return "Slight to Moderate Thunderstom"
+    case 96: return "Thunderstorm with slight hail"
+    case 99: return "Thunderstorm with heavy hail"
+  }
 }
